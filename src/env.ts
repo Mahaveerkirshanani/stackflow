6196d7cb9150cd5c08061c725e9cbac2121d3fbe
MahaveerKirshanani <kumarmahaveer486@gmail.com>
2024-07-23 06:42:53 +0500

    This is our project setup where we have setup our project with appwrite nextjs

---
+++ src/env.ts
@@ -0,0 +1,16 @@
+const env = {
+  appwrite: {
+    endpiont: String(process.env.NEXT_PUBLIC_APPWRITE_HOST_URL),
+    projectId: String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID),
+    apikey: String(process.env.APPWRITE_API_KEY),
+  },
+};
+
+
+
+
+
+
+
+
+export default env
